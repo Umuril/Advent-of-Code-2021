91febd90d09b5ad539da33dd99c87e8c4a87ca6f
Umuril Lyerood <umuril.lyerood@gmail.com>
2021-12-12 16:19:00 +0100

    Cargo clean

--- 01/target/rls/debug/deps/sonar_sweep-8d0bf7eb84987b12.d
+++
@@ -1,5 +0,0 @@
-/home/eddy/Projects/AOC/01/target/rls/debug/deps/sonar_sweep-8d0bf7eb84987b12.rmeta: src/main.rs
-
-/home/eddy/Projects/AOC/01/target/rls/debug/deps/sonar_sweep-8d0bf7eb84987b12.d: src/main.rs
-
-src/main.rs:
